d24165c8c9bfce1d2147f85c4bf2d9bc9a2a662b
Nico Ricaldi <nricaldi.nr@gmail.com>
2025-12-05 15:24:02 -0500

    Add cart icon quantity indicator

--- app/ui/header/cart-icon.tsx
+++ app/ui/header/cart-icon.tsx
@@ -7,23 +7,16 @@ export default function CartIcon() {
   const magnets = useCart();
 
   const totalMagnetCount = useMemo(() => {
-    console.log('new magnets: ', magnets);
-    console.log('unique current length:', magnets.length)
-
-    const count = magnets.reduce((totalMagnetCount, magnet) => {
+    return magnets.reduce((totalMagnetCount, magnet) => {
       return totalMagnetCount += magnet.quantity;
     }, 0);
-
-    console.log('total count:', count);
-
-    return count;
   }, [magnets]);
 
 
   return (
     <div className={style.cartIconWrapper}>
       <LuShoppingCart />
-      <span>{ totalMagnetCount }</span>
+      { totalMagnetCount > 0 && <span className={style.cartQuantity}>{ totalMagnetCount }</span> }
     </div>
   );
 }
