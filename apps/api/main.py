37560956c1a9a4dd28430d94d62bd8ce988df430
Nico Ricaldi <nricaldi.nr@gmail.com>
2025-12-18 11:36:20 -0500

    More shippo investigations

--- apps/api/main.py
+++ apps/api/main.py
@@ -38,7 +38,7 @@ def shippo_test():
         mass_unit=components.WeightUnitEnum.LB
     )
 
-    shipment_create_request = components.ShipmentCreateRequest(
+    shipment_request = components.ShipmentCreateRequest(
         address_from=address_from,
         address_to=address_to,
         parcels=[parcel],
@@ -46,13 +46,27 @@ def shippo_test():
     )
 
     logger.info('Creating shipment')
-    shipment = shippo_sdk.shipments.create(shipment_create_request)
+    shipment = shippo_sdk.shipments.create(shipment_request)
 
-    logger.info(f'{len(shipment.rates)} rates found:')
+    logger.info(f'{len(shipment.rates)} rates found:\n')
     for rate in shipment.rates:
         logger.info(f'Provider: {rate.provider}')
         logger.info(f'Service Level: {rate.servicelevel.name}')
-        logger.info(f'Price: ${rate.amount}\n')
+        logger.info(f'Price: ${rate.amount}')
+        logger.info(f'Estimated days: {rate.estimated_days}\n')
+
+    chosen_rate = shipment.rates[0]
+    logging.info(f'Choosing {chosen_rate.provider} - {chosen_rate.servicelevel.name}')
+
+    logger.info('Creating transaction...')
+    transaction_request = components.TransactionCreateRequest(rate=chosen_rate.object_id, label_file_type=components.LabelFileTypeEnum.PDF, async_=False)
+    transaction = shippo_sdk.transactions.create(transaction_request)
+
+    if transaction.status == 'SUCCESS':
+        logging.info(transaction.label_url)
+        logging.info(transaction.tracking_number)
+    else:
+        logging.error(transaction.messages)
 
 
 if __name__ == ('__main__'):
